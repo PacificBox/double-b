783cc1a084cf65ec35434c1d7d4ffbd391cb0e36
PacificBox <mfungalei@protonmail.com>
2026-09-21 01:10:17 +0930

    Add service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,40 @@
+const CACHE_NAME = 'pacificbox-doubleb-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './PacificBox.png'
+];
+
+// Install Lifecycle Event
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Activate Lifecycle Event
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Fetch Asset Event (Serves from cache first for maximum speed)
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((cachedResponse) => {
+      return cachedResponse || fetch(e.request);
+    })
+  );
+});
